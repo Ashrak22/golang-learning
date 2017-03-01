4a96f1f51b10414f4bc69bc078c1f6e9e45657bc
Ashrak22 <jakub.simon@gmail.com>
2017-03-01 08:40:40 +0100

    Added possibility to change the arg separator

--- src/args/args_main.go
+++ src/args/args_main.go
@@ -4,13 +4,20 @@ type ArgFunc func(...string)
 
 type Argument struct {
 	argsMap map[string]arg
+	separator string
 }
 
 func NewArg() *Argument {
 	argg := new(Argument)
 	argg.argsMap = make(map[string]arg)
+	argg.separator = "--"
 	return argg
 }
+
+func (a *Argument) SetSeparator(sep string) {
+	a.separator = sep
+}
+
 
 func (a *Argument) RegisterArg(name string, argument ArgFunc, count int) bool {
 	_,existed := a.argsMap[name]
