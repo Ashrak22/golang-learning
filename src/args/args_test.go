4adce631870ca329432289e4c977a21a514ea385
Ashrak22 <jakub.simon@gmail.com>
2017-03-14 16:18:24 +0100

    bugfix

--- src/args/args_test.go
+++ src/args/args_test.go
@@ -1,13 +1,14 @@
 package args
 
 import (
+	"bettererror"
 	"testing"
 )
 
 var errorCount int
 
-func help(vs ...string){
-	errorCount = errorCount+1
+func help(vs ...string) {
+	errorCount = errorCount + 1
 }
 
 func TestEvalArgsTooManyParams(t *testing.T) {
@@ -16,7 +17,7 @@ func TestEvalArgsTooManyParams(t *testing.T) {
 	a.RegisterArg("help", ArgFunc(help), 0, "--")
 	var args []string
 	args = append(args, "args", "--help", "abc")
-	err := a.EvalArgs(args).(*ArgError)
+	err := a.EvalArgs(args).(*bettererror.BetterError)
 	if err.Code() != 3 {
 		t.Fatalf("Expected error: Too Many params, received: %s", err.Error())
 	}
@@ -24,24 +25,24 @@ func TestEvalArgsTooManyParams(t *testing.T) {
 
 func TestEvalArgsWrongArg(t *testing.T) {
 	a := NewArg()
-    errorCount = 0
-    a.RegisterArg("help", ArgFunc(help), 0, "--")
+	errorCount = 0
+	a.RegisterArg("help", ArgFunc(help), 0, "--")
 	var args []string
 	args = append(args, "args", "--hepp")
-    err := a.EvalArgs(args).(*ArgError)
+	err := a.EvalArgs(args).(*bettererror.BetterError)
 	if err.Code() != 2 {
-        t.Fatalf("Expected error: Wrong arg, received: %s", err.Error())
-    }
+		t.Fatalf("Expected error: Wrong arg, received: %s", err.Error())
+	}
 }
 
 func TestEvalArgsNoParams(t *testing.T) {
 	a := NewArg()
-    errorCount = 0
-    a.RegisterArg("help", ArgFunc(help), 0, "--")
-    var args []string
-    args = append(args, "args")
-    err := a.EvalArgs(args)
-    if err != nil {
-        t.Fatalf("Expected no error, received: %s", err.Error())
-    }
+	errorCount = 0
+	a.RegisterArg("help", ArgFunc(help), 0, "--")
+	var args []string
+	args = append(args, "args")
+	err := a.EvalArgs(args)
+	if err != nil {
+		t.Fatalf("Expected no error, received: %s", err.Error())
+	}
 }
