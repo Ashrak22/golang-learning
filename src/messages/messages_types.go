021f64bf630e52fda24c25ec3967b7fd211ca011
Ashrak_22 <jakub.simon@gmail.com>
2017-11-16 16:42:26 +0100

    exported types comments

--- src/messages/messages_types.go
+++ src/messages/messages_types.go
@@ -2,6 +2,8 @@ package messages
 
 //MsgUnmarshaller ist the callback for correct Unmarshalling the messages received
 type MsgUnmarshaller func(buffer []byte) error
+
+// MsgError is a function to be called onError
 type MsgError func(err error)
 
 type readInfo struct {
